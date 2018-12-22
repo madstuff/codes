4eac855595df0cecee7c80c571c5f556888b4504
madstuff <madstuffs@outlook.com>
2018-12-22 19:35:59 +0530

    reverse function added

--- hacker-rank/src/com/madstuff/string/StringReverse.java
+++ hacker-rank/src/com/madstuff/string/StringReverse.java
@@ -2,7 +2,19 @@ package com.madstuff.string;
 
 public class StringReverse {
 
+    /**
+     * Checks for Palindrome.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+
+        String name = "naman";
+        String reverse = new StringBuilder(name).reverse().toString();
+
+        System.out.println(reverse);
+
+        System.out.println(name.equalsIgnoreCase(reverse));
 
     }
 }
